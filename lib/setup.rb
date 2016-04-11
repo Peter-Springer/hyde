35e71664341ddee7eb700b92d8d9dcbe7c152eda
Charles Kaminer <ckamine@g.clemson.edu>
2016-04-11 14:10:47 -0600

    initial changes

--- lib/setup.rb
+++ lib/setup.rb
@@ -2,12 +2,11 @@ require 'FileUtils'
 class Setup
 
   def new_project_skeleton(project_file_path)
-    FileUtils.mkdir_p 'project_file_path'
+    
   end
 
   def create_folder(file_name)
-    FileUtils.cd
-    FileUtils.mkdir_p(file_name)
+
   end
   #
   # def create_file
--- test/setup_test.rb
+++ test/setup_test.rb
@@ -7,7 +7,7 @@ require './lib/setup.rb'
 class SetupTest < Minitest::Test
 
   def test_new_project_skeleton_creates_project_file
-    assert_equal  ~/turing/1module/hyde/newproject/version1, Setup.new.new_project_skeleton(~/turing/1module/hyde/newproject/version1)
+
   end
 
   def test_setup_exists
