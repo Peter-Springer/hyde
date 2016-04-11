3607e57a9069a75db700f1edb37ce04d73901208
Charles Kaminer <ckamine@g.clemson.edu>
2016-04-11 14:21:26 -0600

    resolve conflicts

--- test/setup_test.rb
+++ test/setup_test.rb
@@ -7,12 +7,8 @@ require './lib/setup.rb'
 class SetupTest < Minitest::Test
 
   def test_new_project_skeleton_creates_project_file
-<<<<<<< HEAD
     dir = Setup.new
     result = dir.new_project_skeleton() 
-=======
-
->>>>>>> 4f11b67cd26e2b0d14cd5253f47cff19ea9daa4e
   end
 
   def test_setup_exists
