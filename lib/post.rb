929bbf76ca9d324a9f9a535aae84494a037b89db
Peter Springer <peterspringer829@gmail.com>
2016-04-12 21:38:13 -0600

    iteration two complete, plus clean up

---
+++ lib/post.rb
@@ -0,0 +1,38 @@
+require './lib/build.rb'
+
+class Post
+
+  def initialize(file_path, title)
+    @file_path = file_path
+    @title = title
+  end
+
+  def new_post
+    if Dir.exist?(File.join(Dir.pwd, @file_path))
+      format_title
+      create_new_file
+      basic_content
+    else
+      Build.new(@file_path).setup.new_project_skeleton
+      format_title
+      create_new_file
+      basic_content
+    end
+  end
+
+  def format_title
+    @title.gsub(" ", "-")
+  end
+
+  def create_new_file
+    date = Time.new.strftime("%Y-%m-%d")
+    FileUtils.touch (File.join(Dir.pwd, "#{@file_path}/source/posts/#{date}-#{format_title}.md"))
+  end
+
+  def basic_content
+    date = Time.new.strftime("%Y-%m-%d")
+    message = "# #{@title} \n\n Your content here"
+    File.write(File.join(Dir.pwd, "#{@file_path}/source/posts/#{date}-#{format_title}.md"), message)
+  end
+
+end
---
+++ test/post_test.rb
@@ -0,0 +1,40 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/build.rb'
+require './lib/setup.rb'
+require './lib/post.rb'
+
+class PostTest < Minitest::Test
+
+  def test_format_title_returns_title_with_no_spaces
+    skip
+    b = Build.new("/test/newproject")
+    post = Post.new("/test/newproject", "Lets Go")
+    build_project_scaffold(b)
+    post.format_title
+
+    assert_equal "Lets-Go", post.format_title
+
+    FileUtils.remove_dir(File.join(Dir.pwd, "/test/newproject"))
+  end
+
+  def test_basic_content
+    b = Build.new("/test/newproject")
+    post = Post.new("/test/newproject", "Lets Go")
+    build_project_scaffold(b)
+    post.create_new_file
+    post.basic_content
+
+
+  end
+
+
+    def build_project_scaffold(b)
+      b.setup.new_project_skeleton
+      b.copy_source
+    end
+
+
+end
